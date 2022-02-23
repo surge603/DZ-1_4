4d26408894d5a66f1856de407f4063161f964e4e
Artem <40538552+surge603@users.noreply.github.com>
2022-02-23 16:46:01 +0300

    Задача 28

---
+++ tsk28/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 28: Подсчитайте сумму цифр в числе.
+
+Console.Clear();
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+int sum = 0;
+
+while (a > 0)
+{
+    sum += a % 10;
+    a /= 10;
+}
+
+Console.WriteLine($"Сумма цифр в числе: {sum}");

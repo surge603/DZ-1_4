0f42516cfc0cc41ea33e5d8443999636be60a1fb
Artem <40538552+surge603@users.noreply.github.com>
2022-02-23 16:58:02 +0300

    Задача 30

---
+++ tsk30/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 30: Покажите кубы чисел, заканчивающихся на чётную цифру.
+
+Console.Clear();
+Console.Write("Введите максимум цикла: ");
+int max = int.Parse(Console.ReadLine());
+for (int i = 0; i <= max; i++)
+{
+    int cube = i * i * i;
+    int last_dig = cube % 10;
+    if (last_dig / 2 * 2 == last_dig)
+    {
+        Console.WriteLine($"Четный куб: {i}^3 = {cube} ");
+    }
+}

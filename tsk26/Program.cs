cb8e1ac7777c1c6137fee22da5ac2f8d13e8c5c2
Artem <40538552+surge603@users.noreply.github.com>
2022-02-23 16:38:47 +0300

    Задача 26

--- tsk26/Program.cs
+++ tsk26/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 26: Возведите число А в натуральную степень B, используя цикл.
+
+Console.Clear();
+Console.Write("Введите основание степени: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите значание степени: ");
+int b = int.Parse(Console.ReadLine());
+
+int result = 1;
+for (int i = 1; i <= b; i++)
+{
+    result *= a;
+}
+
+Console.WriteLine($"{a}^{b} = {result}");

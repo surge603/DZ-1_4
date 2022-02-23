a488be756369af57f496004172e24335b4e81085
Artem <40538552+surge603@users.noreply.github.com>
2022-02-23 17:09:43 +0300

    Задача 32

---
+++ tsk32/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 32: Задайте массив из восьми элементов, заполненных нулями и единицами. Выведите их на экран
+
+Console.Clear();
+int[] arr = new int[8];
+
+for (int i = 0; i < 8; i++)
+{
+    arr[i] = new Random().Next(0, 2);
+}
+
+for (int i = 0; i < 8; i++)
+{
+    Console.Write($"{arr[i]} ");
+}
+Console.WriteLine();
